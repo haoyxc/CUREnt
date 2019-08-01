5e9e14a0a4af267fa555e5d1b304a0f85684efff
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-01 19:24:32 -0400

    get news

--- routes/GetNews.js
+++ routes/GetNews.js
@@ -1,13 +1,17 @@
 const axios = require("axios");
 const newsType = ["politics", "science", "world", "business"];
-const baseUrl = "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=";
+// const baseUrl = "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=";
 const API_KEY = process.env.NYT_API;
 const baseUrl = `https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${API_KEY}`;
 
 async function getArticles(category) {
-  let resp = await axios(
-    `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${API_KEY}`
-  );
-  console.log(resp);
+  try {
+    let resp = await axios(
+      `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${API_KEY}`
+    );
+    console.log(resp.data.results);
+  } catch (e) {
+    console.log(e);
+  }
 }
 getArticles(newsType[0]);
