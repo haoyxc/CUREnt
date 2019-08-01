372402249fa6600f42d13b8ff3ece37bd04b456b
Derek <derekjou0@gmail.com>
2019-08-01 19:19:54 -0400

    axios continued

--- routes/pullNews.js
+++ routes/pullNews.js
@@ -2,7 +2,13 @@ const axios = require('axios');
 const NY_TIMES_API_KEY = 'L9wS3UQJeTPAD7ZUyum8BTR4RaKpGefV'
 const newsType = ['politics', 'science', 'world', 'business'];
 
+
+//Pull the stories from the specified categories
 axios.get(`https://api.nytimes.com/svc/topstories/v2/${newsType[0]}.json?api-key=${NY_TIMES_API_KEY}`)
     .then(function(response){
-        console.log(response);
+        // Top 20 stories
+        let articles = response.results.slice(0,20); 
+        let abstractAndUrl = [];
+        console.log(articles);
+
     })
