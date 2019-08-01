dc39f873d79a9b0854b3a689a995a722f372295c
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-01 16:09:46 -0400

    mongoose models

---
+++ models/Quiz.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const Questions = require("./Questions");
+
+const quizSchema = new mongoose.Schema({});
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const connect = process.env.MONGODB_URI;
+const Stat = require("./Stat");
+
+mongoose.connect(connect);
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  interests: {
+    type: [String]
+  },
+  stats: {
+    type: mongoose.Schema.ObjectId,
+    ref: Stat
+  }
+});
+
+let User = mongoose.model("User", userSchema);
+module.exports = User;
