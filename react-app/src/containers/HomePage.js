793d3d26c169bc0f2af87d03eb85260e4beb13c2
bsitenga <sitenga@usc.edu>
2019-08-02 00:48:26 -0400

    check for logged in

--- react-app/src/containers/HomePage.js
+++ react-app/src/containers/HomePage.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { Redirect } from 'react-router-dom';
 
 export default function HomePage() {
+  
+  
+
   return <div>HomePage</div>;
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RegisterPage from "./containers/RegisterPage";
-import HomePage from "./containers/HomePage"
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import RegisterPage from './containers/RegisterPage';
+import HomePage from './containers/HomePage';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const checkIfLoggedIn = async () => {
+	const userResponse = await fetch('http://localhost:5000/user', {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json'
+		}
+	});
+	const userContent = await userResponse.json();
+	if (userContent.loggedIn) {
+		return true;
+	}
+	return false;
+};
+
 const routing = (
-    <Router>
-      <Route path="/register" component={RegisterPage}/>
-      <Route path="/homepage" component={HomePage}/>
-    </Router>
-)
+	<Router>
+        <Route path="/register" component={RegisterPage} />
+		{checkIfLoggedIn() && <Route path="/homepage" component={HomePage} />}
+	</Router>
+);
 
 ReactDOM.render(routing, document.getElementById('root'));
 
