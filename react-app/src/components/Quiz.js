243362fdd19d21258e8d3e149c7ede73b9d2571f
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-08-02 06:02:55 -0400

    frontend work

--- react-app/src/components/Quiz.js
+++ react-app/src/components/Quiz.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const axios = require("axios");
+const _ = require("underscore");
+const QuizItem = require("./QuizItem");
 
-export default function Quiz() {
-  return <div>Quiz</div>;
+export default function Quiz({ questions }) {
+  const [questionsObj, setQuestionsObj] = useState(null);
+
+  useEffect(() => {
+    let newQuestionArr = questions.map(q => {
+      let allAnswers = [...q.wrongAnswers, ...q.correctAnswer];
+
+      return {
+        question: q.correctAnswer,
+        allAnswers: _.shuffle(allAnswers),
+        correctAnswer: q.correctAnswer,
+        sourceLink: q.sourceLink
+      };
+    });
+    setQuestionsObj(newQuestionArr);
+  }, []);
+  return (
+    <div>
+      {questionsObj.map((qObj, index) => (
+        <QuizItem questionItem={qObj} index={index} />
+      ))}
+    </div>
+  );
 }
