85151421d83e3254cce5a7f70276f7297cbcd3a1
bsitenga <sitenga@usc.edu>
2019-08-02 09:43:08 -0400

    merge conflicts

--- react-app/src/containers/HomePage.js
+++ react-app/src/containers/HomePage.js
@@ -13,113 +13,6 @@ const parseJwt = token => {
 export default function HomePage() {
 	const [questions, setQuestions] = useState([]);
 
-<<<<<<< HEAD
-	const getAllQuestions = async () => {
-		try {
-			let response = await fetch("http://localhost:5000/quiz", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			response = await response.json();
-			setQuestions(response[Math.floor(Math.random() * 3)]);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const getBusinessQuestions = async () => {
-		try {
-			let response = await fetch("http://localhost:5000/quiz/business", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			response = await response.json();
-			setQuestions(response);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const getPoliticsQuestions = async () => {
-		try {
-			let response = await fetch("http://localhost:5000/quiz/politics", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			response = await response.json();
-			setQuestions(response);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const getTechQuestions = async () => {
-		try {
-			let response = await fetch("http://localhost:5000/quiz/technology", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			response = await response.json();
-			setQuestions(response);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	const getWorldQuestions = async () => {
-		try {
-			let response = await fetch("http://localhost:5000/quiz/world", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			response = await response.json();
-			setQuestions(response);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	// console.log(localStorage.getItem("token"));
-
-	return (
-		<div>
-			<h1>HomePage</h1>
-			<div style={styles.row}>
-				<button onClick={() => getAllQuestions()}>Everything</button>
-				<button onClick={() => getBusinessQuestions()}>Business</button>
-				<button onClick={() => getTechQuestions()}>Technology</button>
-			</div>
-			<div style={styles.row}>
-				<button onClick={() => getPoliticsQuestions()}>Politics</button>
-				<button onClick={() => getWorldQuestions()}>World</button>
-			</div>
-			{questions.length ? <div>{questions[0].question}</div> : null}
-			<Stats user={parseJwt(localStorage.getItem("token")).username} />
-		</div>
-	);
-}
-
-const styles = {
-	row: {
-		flex: 1,
-		alignItems: "center",
-	},
-	catSel: {
-		height: 50,
-		width: 40,
-		borderRadius: 5,
-	},
-=======
   const getAllQuestions = async () => {
     try {
       let response = await fetch("http://localhost:5000/quiz", {
@@ -227,5 +120,4 @@ const styles = {
     width: 40,
     borderRadius: 5
   }
->>>>>>> 1b1da0f4447e84daadfb8eeb27e5172a266c6c7e
 };
