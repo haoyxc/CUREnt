e8837bdda97a61022201a2760fab0132c2dbdceb
bsitenga <sitenga@usc.edu>
2019-08-02 09:15:32 -0400

    produced the cards

--- react-app/src/components/QuizCards.js
+++ react-app/src/components/QuizCards.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function QuizCards({allQuestions}) {
-    let questionArray = [];
+export default function QuizCards({ allQuestions }) {
+    
+    let allArray = [];
     for (let i = 0; i < allQuestions.length; i++) {
-        questionArray.push(allQuestions[i].question)
+        let ansArray = [allQuestions[i].correctAnswer];
+        for (let j = 0; j < allQuestions[i].wrongAnswers.length; j++) {
+            ansArray.push(allQuestions[i].wrongAnswers[j]);
+        }
+        ansArray.sort(() => Math.random() - 0.5);
+        allArray.push({question: allQuestions[i].question, answerArray: ansArray});
     }
-    console.log(questionArray);
-    return (<div>
-        <ul>
-            {allQuestions.map(item => {
-             return <li>{item.question}</li>   
-            })}
-        </ul>
-    </div>)
+    
+	return (
+		<div>
+			{allArray.map((item) => {
+				return (
+					<div className="card question-card" style={{width: 500}}>
+						<div className="card-body">
+							<h5 className="card-title">{item.question}</h5>
+                            <form>
+                                {item.answerArray.map(ans => {
+                                    return <input type="button" className="answerBtn" value={ans} />
+                                })}
+                            </form>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
